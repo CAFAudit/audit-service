8290fc2e6721905fe9a5163ebbe6e8ac7e2d163d
Connor Mulholland <connor.mulholland@hpe.com>
2016-02-16 15:59:30 +0000

    Added multi-tenancy support

--- src/test/java/com/hpe/caf/services/audit/client/ApiClientTest.java
+++ src/test/java/com/hpe/caf/services/audit/client/ApiClientTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by crooksph on 04/02/2016.
@@ -31,5 +33,41 @@ public class ApiClientTest {
 
         exception.expect(Exception.class);
         client.applicationsPost(auditEventsDefFile);
+    }
+
+    @Test
+    public void testTenantsPostNullTenant() throws Exception {
+        DefaultApi client = new DefaultApi();
+
+        List<String> applications = new ArrayList<String>();
+        applications.add("TestApplication1");
+        applications.add("TestApplication2");
+
+        exception.expect(ApiException.class);
+        client.tenantsPost(null,applications);
+    }
+
+    @Test
+    public void testTenantsPostNullApplication() throws Exception {
+        DefaultApi client = new DefaultApi();
+        client.getApiClient().setBasePath("http://non.existent.host/v1");
+
+        String tenant = "TestTenant";
+
+        exception.expect(ApiException.class);
+        client.tenantsPost(tenant, null);
+    }
+
+    @Test
+    public void testTenantsPostNonexistentHost() throws Exception {
+        DefaultApi client = new DefaultApi();
+
+        String tenant = "TestTenant";
+        List<String> applications = new ArrayList<String>();
+        applications.add("TestApplication1");
+        applications.add("TestApplication2");
+
+        exception.expect(Exception.class);
+        client.tenantsPost(tenant, applications);
     }
 }

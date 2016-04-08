764b39ec9a5ec1b44743a5a92fbcc4b0bd766c46
Connor Mulholland <connor.mulholland@hpe.com>
2016-04-08 09:51:08 +0100

    CAF-833: parameters now passed in the body request of the POST /tenants call

--- src/test/java/com/hpe/caf/services/audit/client/ApiClientTest.java
+++ src/test/java/com/hpe/caf/services/audit/client/ApiClientTest.java
@@ -2,6 +2,7 @@ package com.hpe.caf.services.audit.client;
 
 import com.hpe.caf.services.audit.client.api.ApplicationsApi;
 import com.hpe.caf.services.audit.client.api.TenantsApi;
+import com.hpe.caf.services.audit.client.model.NewTenant;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -37,7 +38,7 @@ public class ApiClientTest {
     }
 
     @Test
-    public void testTenantsPostNullTenant() throws Exception {
+    public void testTenantsPostNullNewTenant() throws Exception {
         TenantsApi client = new TenantsApi();
 
         List<String> applications = new ArrayList<String>();
@@ -45,18 +46,7 @@ public class ApiClientTest {
         applications.add("TestApplication2");
 
         exception.expect(ApiException.class);
-        client.tenantsPost(null,applications);
-    }
-
-    @Test
-    public void testTenantsPostNullApplication() throws Exception {
-        TenantsApi client = new TenantsApi();
-        client.getApiClient().setBasePath("http://non.existent.host/v1");
-
-        String tenant = "TestTenant";
-
-        exception.expect(ApiException.class);
-        client.tenantsPost(tenant, null);
+        client.tenantsPost(null);
     }
 
     @Test
@@ -68,7 +58,11 @@ public class ApiClientTest {
         applications.add("TestApplication1");
         applications.add("TestApplication2");
 
+        NewTenant newTenant = new NewTenant();
+        newTenant.setTenantId(tenant);
+        newTenant.setApplication(applications);
+
         exception.expect(Exception.class);
-        client.tenantsPost(tenant, applications);
+        client.tenantsPost(newTenant);
     }
 }

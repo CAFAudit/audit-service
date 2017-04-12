035b3468f9956dac2637e96c5b785139c899d46a
Connor Mulholland <connor.mulholland@hpe.com>
2017-04-12 20:43:29 +0100

    CAF-2273: Removal of Kafka from caf-audit project.

--- caf-audit/src/main/java/com/hpe/caf/auditing/AuditConnectionFactory.java
+++ caf-audit/src/main/java/com/hpe/caf/auditing/AuditConnectionFactory.java
@@ -17,18 +17,17 @@ package com.hpe.caf.auditing;
 
 import com.hpe.caf.api.ConfigurationException;
 import com.hpe.caf.api.ConfigurationSource;
-import com.hpe.caf.auditing.kafka.KafkaAuditConnection;
 
 public class AuditConnectionFactory {
 
     /**
-     * Create Kafka connection for the Audit application using the specified list of seed brokers.
+     * Create connection for the Audit application.
      *
      * @param configSource the configuration source
      * @return the connection to the audit server
      * @throws ConfigurationException if the audit server details cannot be retrieved from the configuration source
      */
     public static AuditConnection createConnection(final ConfigurationSource configSource) throws ConfigurationException {
-        return new KafkaAuditConnection(configSource);
+        return null;
     }
 }

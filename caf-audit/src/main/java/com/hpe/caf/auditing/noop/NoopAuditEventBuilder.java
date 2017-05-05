463deb86d4dac4154f595a35f97993bb9447d58e
Dermot Hardy <dermot.hardy@hpe.com>
2017-05-05 21:10:13 +0100

    Added a no-op implementation

---
+++ caf-audit/src/main/java/com/hpe/caf/auditing/noop/NoopAuditEventBuilder.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2015-2017 Hewlett Packard Enterprise Development LP.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hpe.caf.auditing.noop;
+
+import com.hpe.caf.auditing.AuditEventBuilder;
+import java.util.Date;
+
+final class NoopAuditEventBuilder implements AuditEventBuilder
+{
+    public NoopAuditEventBuilder()
+    {
+    }
+
+    @Override
+    public void setApplication(final String applicationId)
+    {
+    }
+
+    @Override
+    public void setUser(final String userId)
+    {
+    }
+
+    @Override
+    public void setTenant(final String tenantId)
+    {
+    }
+
+    @Override
+    public void setCorrelationId(final String correlationId)
+    {
+    }
+
+    @Override
+    public void setEventType(final String eventCategoryId, final String eventTypeId)
+    {
+    }
+
+    @Override
+    public void addEventParameter(final String name, final String columnName, final String value)
+    {
+    }
+
+    @Override
+    public void addEventParameter(final String name, final String columnName, final short value)
+    {
+    }
+
+    @Override
+    public void addEventParameter(final String name, final String columnName, final int value)
+    {
+    }
+
+    @Override
+    public void addEventParameter(final String name, final String columnName, final long value)
+    {
+    }
+
+    @Override
+    public void addEventParameter(final String name, final String columnName, final float value)
+    {
+    }
+
+    @Override
+    public void addEventParameter(final String name, final String columnName, final double value)
+    {
+    }
+
+    @Override
+    public void addEventParameter(final String name, final String columnName, final boolean value)
+    {
+    }
+
+    @Override
+    public void addEventParameter(final String name, final String columnName, final Date value)
+    {
+    }
+
+    @Override
+    public void send() throws Exception
+    {
+    }
+}

c4a582c0a17dc0a67d46f0c335813a681b010cb3
Anthony McGreevy <anthony.mcgreevy@microfocus.com>
2019-02-25 15:01:35 +0000

    Code tidy

--- caf-audit-service/src/main/java/com/hpe/caf/services/audit/server/api/impl/AuditeventsApiServiceImpl.java
+++ caf-audit-service/src/main/java/com/hpe/caf/services/audit/server/api/impl/AuditeventsApiServiceImpl.java
@@ -16,18 +16,13 @@
 package com.hpe.caf.services.audit.server.api.impl;
 
 import com.hpe.caf.api.ConfigurationException;
-import com.hpe.caf.api.ConfigurationSource;
 import com.hpe.caf.auditing.AuditConnection;
 import com.hpe.caf.auditing.AuditChannel;
 import com.hpe.caf.auditing.AuditConnectionFactory;
 import com.hpe.caf.auditing.AuditCoreMetadataProvider;
 import com.hpe.caf.auditing.AuditEventBuilder;
 import com.hpe.caf.auditing.AuditIndexingHint;
-import com.hpe.caf.auditing.elastic.ElasticAuditConfiguration;
 import com.hpe.caf.services.audit.server.api.exceptions.BadRequestException;
-import org.springframework.beans.factory.support.RootBeanDefinition;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 import java.text.DateFormat;
 import java.text.ParseException;

6c794d08044fe8abe189a30cee69406016584847
Anthony McGreevy <anthony.mcgreevy@microfocus.com>
2019-03-06 00:00:11 +0000

    Updated license header

--- caf-audit/src/main/java/com/hpe/caf/auditing/exception/AuditingImplementationException.java
+++ caf-audit/src/main/java/com/hpe/caf/auditing/exception/AuditingImplementationException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 EntIT Software LLC, a Micro Focus company.
+ * Copyright 2015-2018 Micro Focus or one of its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

f6460b65842a1a8375e3c209d33c7681c90a36a0
thilagasanthosh <thilagavathi.santhoshkumar@microfocus.com>
2019-08-19 14:48:24 +0100

    SCMOD-7032: order updated

--- deploy/src/main/resources/production-swarm/environment.sh
+++ deploy/src/main/resources/production-swarm/environment.sh
@@ -8,11 +8,11 @@ export CAF_AUDIT_SERVICE_PORT=25080
 ###
 # Elasticsearch
 ###
+## A comma separated list of Elasticsearch HOST values.
+export CAF_ELASTIC_HOST_VALUES=192.168.56.10
 ## The number of primary shards that an Elasticsearch index should have.
 export CAF_ELASTIC_NUMBER_OF_SHARDS=5
 ## The number of replica shards (copies) that each primary shard should have.
 export CAF_ELASTIC_NUMBER_OF_REPLICAS=1
-## A comma separated list of Elasticsearch HOST values.
-export CAF_ELASTIC_HOST_VALUES=192.168.56.10
 ## The REST port of the Elasticsearch server listens on.
 export CAF_ELASTIC_PORT_VALUE=9200

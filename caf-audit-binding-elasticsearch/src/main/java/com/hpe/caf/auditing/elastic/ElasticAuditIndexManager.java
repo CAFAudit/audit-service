b58f13462bfe2039da320eb2425e469cd516a551
Anthony McGreevy <anthony.mcgreevy@microfocus.com>
2019-08-20 12:06:09 +0100

    Updated

--- caf-audit-binding-elasticsearch/src/main/java/com/hpe/caf/auditing/elastic/ElasticAuditIndexManager.java
+++ caf-audit-binding-elasticsearch/src/main/java/com/hpe/caf/auditing/elastic/ElasticAuditIndexManager.java
@@ -32,7 +32,7 @@ import org.elasticsearch.common.xcontent.XContentType;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
-import org.elasticsearch.client.indices.GetIndexTemplatesRequest;
+import org.elasticsearch.client.indices.IndexTemplatesExistRequest;
 import org.elasticsearch.client.indices.GetIndexTemplatesResponse;
 
 public class ElasticAuditIndexManager {

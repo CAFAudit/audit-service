64d86dc2c12c287b0a7f40f9c5558b677063dc43
Anthony McGreevy <anthony.mcgreevy@microfocus.com>
2019-08-28 14:18:58 +0100

    Made class final

--- caf-audit-binding-elasticsearch/src/main/java/com/hpe/caf/auditing/elastic/ElasticAuditIndexManager.java
+++ caf-audit-binding-elasticsearch/src/main/java/com/hpe/caf/auditing/elastic/ElasticAuditIndexManager.java
@@ -34,7 +34,7 @@ import java.io.InputStream;
 import java.util.Arrays;
 import org.elasticsearch.client.indices.IndexTemplatesExistRequest;
 
-public class ElasticAuditIndexManager {
+public final class ElasticAuditIndexManager {
 
     private static final Logger LOG = LogManager.getLogger(ElasticAuditIndexManager.class.getName());
     private static final String INDEX_TEMPLATE_NAME = "caf_audit_template";
